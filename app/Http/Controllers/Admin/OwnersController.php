ffb1f1a71390c24bd05f18bec97e04ae34586e6c
{km} <{k3042227625n@gmail.com}>
2024-12-03 20:49:33 +0900

    リサイズなしアップロード

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -14,11 +14,6 @@ use Illuminate\Support\Facades\Log;
 
 class OwnersController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function __construct()
     {
         $this->middleware('auth:admin');
@@ -48,22 +43,11 @@ class OwnersController extends Controller
         dd('オーナー一覧です');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('admin.owners.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         // $request->name;
@@ -104,23 +88,11 @@ class OwnersController extends Controller
             ]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         $owner = Owner::findOrFail($id);
@@ -128,13 +100,6 @@ class OwnersController extends Controller
         return view('admin.owners.edit', compact('owner'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         $owner = Owner::findOrFail($id);
@@ -148,12 +113,6 @@ class OwnersController extends Controller
             ->with(['message' => 'オーナー情報を更新しました。', 'status' => 'info']);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         Owner::findOrFail($id)->delete(); //ソフトデリート
