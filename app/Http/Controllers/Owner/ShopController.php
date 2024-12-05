049d7a5f1ca71085581e3251cdc324bff225e7a4
{km} <{k3042227625n@gmail.com}>
2024-12-06 07:48:57 +0900

    intervention/image

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Shop;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use InterventionImage;
 
 class ShopController extends Controller
 {
@@ -14,13 +15,13 @@ class ShopController extends Controller
     {
         $this->middleware('auth:owners'); //オーナーの確認
 
-        $this->middleware(function($request, $next){
+        $this->middleware(function ($request, $next) {
             $id = $request->route()->parameter('shop'); //shopのid取得
-            if(!is_null($id)){ //null判定
+            if (!is_null($id)) { //null判定
                 $shopsOwnerId = Shop::findOrFail($id)->owner->id;
                 $shopId = (int)$shopsOwnerId; // キャスト 文字列->数値に型変換
                 $ownerId = Auth::id();
-                if($shopId !== $ownerId){ // 同じでなかったら
+                if ($shopId !== $ownerId) { // 同じでなかったら
                     abort(404); // 404画面表示
                 }
             }
@@ -39,18 +40,24 @@ class ShopController extends Controller
     public function edit($id)
     {
         $shop = Shop::findOrFail($id);
-        
+
         return view('owner.shops.edit', compact('shop'));
     }
 
     public function update(Request $request, $id)
     {
         $imageFile = $request->image;
-        if(!is_null($imageFile) && $imageFile->isValid() ){
-            Storage::putFile('public/shops', $imageFile);    
+        if (!is_null($imageFile) && $imageFile->isValid()) {
+            // Storage::putFile('public/shops', $imageFile);
+            $fileName = uniqid(rand() . '_');
+            $extension = $imageFile->extension();
+            $fileNameToStore = $fileName . '.' . $extension;
+            $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+            // dd($imageFile, $resizedImage);
+
+            Storage::put('public/shops/' . $fileNameToStore, $resizedImage);
         }
 
         return redirect()->route('owner.shops.index');
-
     }
 }
