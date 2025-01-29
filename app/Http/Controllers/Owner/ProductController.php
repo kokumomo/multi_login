8608fc6585ac4f964caab5d3b85ffcc52367508b
{km} <{k3042227625n@gmail.com}>
2025-01-30 07:55:30 +0900

    product destroy

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -14,6 +14,7 @@ use App\Models\PrimaryCategory;
 use App\Models\Owner;
 use App\Http\Requests\ProductRequest;
 
+
 class ProductController extends Controller
 {
     public function __construct()
@@ -170,10 +171,10 @@ class ProductController extends Controller
                     $product->is_selling = $request->is_selling;
                     $product->save();
 
-                    if ($request->type === '1') {
+                    if ($request->type === \Constant::PRODUCT_LIST['add']) {
                         $newQuantity = $request->quantity;
                     }
-                    if ($request->type === '2') {
+                    if ($request->type === \Constant::PRODUCT_LIST['reduce']) {
                         $newQuantity = $request->quantity * -1;
                     }
                     Stock::create([
@@ -204,6 +205,11 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Product::findOrFail($id)->delete();
+
+        return redirect()
+        ->route('owner.products.index')
+        ->with(['message' => '商品を削除しました。',
+        'status' => 'alert']);
     }
 }
