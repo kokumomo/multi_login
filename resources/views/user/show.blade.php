f62e5da20532d86154380091bebd27a74f165c8f
{km} <{k3042227625n@gmail.com}>
2025-02-10 07:10:23 +0900

    cart

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -65,7 +65,7 @@
                 <div>
                   <span class="title-font font-medium text-2xl text-gray-900">{{ number_format($product->price) }}</span><span class="text-sm text-gray-700">円(税込)</span>
                 </div>
-                <form method="" action="">
+                <form method="post" action="{{ route('user.cart.add') }}">
                   @csrf
                   <div class="flex items-center">
                     <span class="mr-3">数量</span>
